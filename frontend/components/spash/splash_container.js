75f60289c059956fb87d836fe92dd45134336ea0
yangel20 <61720403+yangel20@users.noreply.github.com>
2020-11-17 20:49:33 -0500

    done with back end user auth

---
+++ frontend/components/spash/splash_container.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+
+import { logout } from '../../actions/session_actions';
+import splash from './splash';
+
+const mSTP = ({ session, entities: { users } }) => {
+  return {
+    currentUser: users[session.id]
+  };
+};
+
+const mDTP = dispatch => ({
+  logout: () => dispatch(logout())
+});
+
+export default connect(
+  mSTP,
+  mDTP
+)(splash);
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Hello, and welcome to Dreamer</h1>, root);
+  const store = configureStore();
+  ReactDOM.render(<Root store={store} />, root);
+  
+    //Testing
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
 });
