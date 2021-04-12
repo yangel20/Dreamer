6923dbe770b8db5a4e967378f53b285470bce19a
yangel20 <61720403+yangel20@users.noreply.github.com>
2021-04-12 12:25:40 -0400

    photo mvp

--- frontend/components/explore/photo_add.jsx
+++ frontend/components/explore/photo_add.jsx
@@ -10,7 +10,6 @@ class PhotoAdd extends React.Component {
             <div className="btn-upload-container">
                 <div className="btn-words">Choose Photo to upload</div>
                 <input className="btn-upload" type="file" accept=".png .jpg .jpeg" multiple />
-
             </div>
         )
     }
