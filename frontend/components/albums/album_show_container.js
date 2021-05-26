26cabc53f501fae61f5e291473f935bbf108f579
yangel20 <61720403+yangel20@users.noreply.github.com>
2021-05-26 10:37:19 -0400

    fix album show file name

--- frontend/components/albums/album_show_container.js
+++ frontend/components/albums/album_show_container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { fetchAlbum } from '../../actions/albums_action';
-import AlbumShow from './album_Show';
+import AlbumShow from './album_show';
 
 const mSTP = (state, ownProps) => {
     return {
